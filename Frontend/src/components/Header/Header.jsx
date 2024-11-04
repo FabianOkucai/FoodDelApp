398b2d3a197f6c393c277ce673c14047f360acc7
Fabian Okucai. <okucaifabian@gmail.com>
2024-11-04 20:41:18 +0300

    basic file

---
+++ Frontend/src/components/Header/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className='header-contents'>
+
+            <h2>Order your favourite food here</h2>
+
+            <p>Welcome to FoodDel, your go-to app for quick and delicious meals delivered right to your door. From local dishes to international flavors, we've got it all!</p>
+
+            <p>With easy ordering, real-time tracking, and flexible payment options, we make food delivery fast and simple. Sign up now and enjoy great deals and speedy service!</p>
+
+            <button>View Menu</button>
+
+        </div>
+
+    </div>
+
+  )
+}
+
+export default Header
